12cabee209d370ef0ff9a46455428d7323f75d60
AyanaFujimoto <ayana.fujimoto01@g.softbank.co.jp>
2020-02-18 13:12:57 +0000

    for heroku

--- app/controllers/question_answer_controller.rb
+++ app/controllers/question_answer_controller.rb
@@ -5,6 +5,7 @@ class QuestionAnswerController < ApplicationController
     @q_length = session[:question_ids].length
     # binding.pry
     @current_q_num = session[:question_ids].index(params[:id].to_i)
-
+    
+    @user_answer_id = params[:session][:user_answer].to_i
   end
 end
