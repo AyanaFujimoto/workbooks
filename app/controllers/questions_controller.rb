d4d8737d8b2f6987af8ed7ea5917444aeaca1be6
AyanaFujimoto <ayana.fujimoto01@g.softbank.co.jp>
2020-02-18 13:30:57 +0000

    updated

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,16 +2,6 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.order(id: :asc).page(params[:page]).per(5)
   end
-
-  def show
-    # @questions_count = session[:question_num]
-    
-    # q_ids = Question.all.pluck(:id)
-    # @shuffled_ids = q_ids.shuffle
-    # @questions = @shuffled_ids[0].tap do |qid|  
-    #   Question.find(qid)
-    # end
-  end
   
   private
   def question_params
